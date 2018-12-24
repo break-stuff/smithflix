c8945c6eba1fc5d424ce36c278fc1b7802d044f7
Burton Smith <bsmith@amazazing.com>
2018-12-24 00:04:54 -0500

    added custom pages and formatting.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <router-link class="navbar-brand" to="/" exact>Smithflix</router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link class="nav-item nav-link" to="/" exact>Home</router-link>
+                    <router-link class="nav-item nav-link" to="/about">About</router-link>
+                </div>
+                <div class="navbar-nav ml-auto">
+                    <router-link class="nav-item nav-link" to="/cart"><i class="fas fa-shopping-cart"></i></router-link>
+                </div>
+            </div>
+        </nav>
+        <div id="nav">
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    padding: 30px;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
---
+++ src/views/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="cart">
+        <h1>Cart</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Cart',
+        data() {
+            return {
+
+            };
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
