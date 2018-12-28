cfc20a1070c1965b4c7f843def83feec3ecb458f
Burton Smith <bsmith@amazazing.com>
2018-12-28 13:38:57 -0500

    Added movie summaries to Dashboard.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,24 @@
 <template>
     <div class="dashboard">
-        <h1>This is the Dashboard page</h1>
+        <h1 class="sr-only">This is the Dashboard page</h1>
+        <movie-list :movies="movies" />
     </div>
 </template>
+
+<script>
+    import movies from '../data/movies';
+    import MovieList from '../components/MovieList';
+
+    export default {
+        name: 'DashBoard',
+        components: {
+            MovieList
+        },
+        data() {
+            return {
+                movies
+            }
+        }
+    }
+</script>
+
