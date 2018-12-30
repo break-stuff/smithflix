1975e66e85fa90c5be4d2a909c3f415f2dd7d73a
Burton Smith <bsmith@amazazing.com>
2018-12-29 20:06:47 -0500

    Added SideNav component

---
+++ src/components/SideNav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="side-nav">
+        <!-- Search -->
+        <!-- Genres -->
+        <!-- Favorites -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SideNav',
+
+    }
+</script>
