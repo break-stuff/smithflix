121e0a8872cfa9696e75d9f53f7e4a728bf1a740
Burton Smith <bsmith@amazazing.com>
2019-01-01 10:53:32 -0500

    Added Genres and Favorites links to SideNav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -2,13 +2,35 @@
     <div class="side-nav">
         <!-- Search -->
         <!-- Genres -->
+        <div class="genres">
+            <h5>Genres</h5>
+
+            <div class="genre form-check" v-for="genre in genres" :key="genre.id">
+                <input type="checkbox" class="form-check-input" v-model="selectedGenres" :value="genre.id" name="" :id="genre.id" /><label :for="genre.id" class="form-check-label">{{ genre.name }}</label>
+            </div>
+        </div>
+
         <!-- Favorites -->
+        <div class="favorites">
+            <h5>Favorites</h5>
+
+            <router-link to="/dashboard/bookmarks"><i class="fas fa-bookmark"></i>Bookmarks</router-link>
+            <router-link to="/dashboard/favorites"><i class="fas fa-heart"></i>Favorites</router-link>
+        </div>
+
     </div>
 </template>
 
 <script>
+    import genres from '../data/genres';
+
     export default {
         name: 'SideNav',
-
+        data() {
+            return {
+                genres,
+                selectedGenres: []
+            }
+        }
     }
 </script>
