e2b41e65b5c4689bdbb313b6f74d2dee81619935
BWSmith <BWsmith@BSmith-MAC.local>
2019-01-08 19:35:28 -0500

    Fixed dashboard route

--- src/router.js
+++ src/router.js
@@ -16,12 +16,14 @@ let router = new Router({
         {
             path: '/dashboard',
             name: 'dashboard',
-            component: () => import( /* webpackChunkName: "dashboard" */ './views/Dashboard.vue')
-        },
-        {
-            path: '/dashboard/:filter',
-            name: 'dashboard',
-            component: () => import( /* webpackChunkName: "dashboard" */ './views/Dashboard.vue')
+            component: () => import( /* webpackChunkName: "dashboard" */ './views/Dashboard.vue'),
+            children: [
+                {
+                    path: ':filter',
+                    name: 'dashboard',
+                    component: () => import( /* webpackChunkName: "dashboard" */ './views/Dashboard.vue')
+                }
+            ]
         },
         {
             path: '/movie/:id',
